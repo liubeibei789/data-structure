7b59aa244352b45ebc0cc0ade5b37a59c231c21e
Beibei LIU <liubeibei789@gmail.com>
2019-04-10 13:30:35 -0700

    Add files via upload

---
+++ delNodeFromLinkedList.c
@@ -0,0 +1,126 @@
+
+// original linked list: 6->5->8->2->NULL
+
+// when running:
+// gcc delNodeFromLinkedList.c
+// ./a.out
+
+ 
+/* outcome:
+[beliu: ~/Desktop]$./a.out
+6->5->8->2->NULL
+deleting 6
+5->8->2->NULL
+deleting 8
+5->2->NULL
+deleting 2
+5->NULL
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+   int value;
+   struct node* next;
+};
+
+
+void printing(struct node* head)
+{
+   struct node *cur = head;
+
+   while(cur != NULL)
+   {
+      printf("%d->",cur->value);
+      cur = cur->next;
+   }
+   printf("NULL\n");
+}
+
+struct node* myLinkDel(struct node* head, int val)
+{
+   struct node *cur = head;
+   if(head == NULL) return NULL;
+   
+   if(head->value == val)  /// reason to introduce dummy is too avoid this corner case....
+   {                        /// seems that there is no way to get rid of both...? (dummy and this corner case)
+      printf("deleting %d\n", head->value);
+      return head->next;
+   }
+
+   
+   while (cur->next!= NULL) 
+   {
+      if (val == cur->next->value)
+      {
+         struct node *tmp = cur->next;
+         printf("deleting %d\n", cur->next->value);
+         cur->next = cur->next->next;
+         
+         free(tmp);
+         break;
+      }
+      cur = cur->next;
+   }
+   return head;
+}
+
+
+void cleanUp(struct node *head)
+{
+   struct node *cleanPtr = head;
+   while(cleanPtr != NULL)
+   {
+      struct node *temp = cleanPtr;
+      cleanPtr = cleanPtr->next;
+      free(temp);
+   }
+}
+
+
+int main()
+{
+   struct node *test1 = malloc(sizeof(struct node));
+   test1->value = 6;
+
+   struct node *testPtr = test1;
+   testPtr->next = malloc(sizeof(struct node));
+   testPtr->next->value = 5;
+   testPtr = testPtr->next;
+
+   
+   testPtr->next = malloc(sizeof(struct node));
+   testPtr->next->value = 8;
+   testPtr = testPtr->next;
+
+   
+   testPtr->next = malloc(sizeof(struct node));
+   testPtr->next->value = 2;
+   testPtr = testPtr->next;
+
+   testPtr->next = NULL;
+
+   printing(test1);
+   test1 = myLinkDel(test1,6); //del head
+   printing(test1);
+
+   test1 = myLinkDel(test1,8); //del middle one
+   printing(test1);
+
+   test1 = myLinkDel(test1,2);  // del last one
+   printing(test1);
+
+   test1 = myLinkDel(test1,5); // del only one 
+   printing(test1);
+   
+   test1 = myLinkDel(test1,9);  // del NULL
+   printing(test1);
+
+   cleanUp(test1);
+ 
+
+
+}
